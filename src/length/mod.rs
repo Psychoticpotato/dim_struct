f2761e723d31af943b79b2b67deb215183e86998
william <wcarroll@localhost.localdomain>
2019-02-16 15:17:52 -0600

    Update Dim into Length and implement the traits

--- src/length/mod.rs
+++ src/length/mod.rs
@@ -1,149 +1,82 @@
-use crate::common::{Float, RoundTo, Unit};
+use crate::base_types::*;
+use crate::common::Float;
+use crate::length::units::LengthUnit;
 use std::ops::{Add, AddAssign, Sub, SubAssign};
+
 #[cfg(test)]
 mod test;
 pub mod units;
-
-use units::si::*;
-#[derive(Clone, Copy)]
-/// A struct to store a floating point number, and a unit
-/// The benefit from this is conversion safety and various methods
+#[derive(Copy, Clone)]
+/// Length handles, well, length.
 ///
-/// **IMPORTANT**:
-///
-/// This is `Clone` and `Copy`.  
-///
-/// Be careful, as after a move, the two instances are no longer tied.
-/// TODO: Should this be Clone and Copy or not?
-pub struct Dim {
+/// Feet, meters, lightyears, etc.
+pub struct Length {
+    /// The unit of this value
+    unit: &'static LengthUnit,
+    /// The value stored as a float
     val: Float,
-    unit: &'static Unit,
 }
-// Begin with static functions
-impl Dim {
-    /// Create a new Dimension with the given value and unit
-    pub fn new(val: Float, unit: &'static Unit) -> Dim {
-        Dim { val, unit }
-    }
-    /// Convert the value from and to the given units
-    pub fn convert(val: Float, from: &'static Unit, to: &'static Unit) -> Float {
-        val / from.in_base * to.in_base
+impl Length {
+    pub fn new(val: Float, unit: &'static LengthUnit) -> Length {
+        Length { val, unit }
     }
 }
-
-// Getters in this one
-impl Dim {
-    /// Returns the value in the current unit
-    pub fn get_val(&self) -> Float {
+// Implement the measurement traits
+impl MeasureTrait for Length {
+    type Unit = LengthUnit;
+    fn get_val(&self) -> Float {
         self.val
     }
-
-    /// Returns the value stored in the specified unit (without mutating)
-    pub fn get_val_as(&self, unit: &'static Unit) -> Float {
-        Dim::convert(self.val, self.unit, unit)
+    fn get_unit(&self) -> &'static LengthUnit {
+        self.unit
     }
-}
-
-// Various display functions here
-impl Dim {
-    /// The value is rounded to the number of decimals (and will show trailing `0`)
-    /// value = 1.5, decimals = 2, unit = "METRE"; result = `1.50m`
-    pub fn display_abbr(&self, decimals: usize) -> String {
-        let val = self.val.round_to(decimals);
-        format!("{:.*}{}", decimals, val, self.unit.abbr)
-    }
-    /// Displays the value with the appropriate singular or plural name after it
-    /// The value is rounded to the number of decimals (and will show trailing `0`)
-    /// If plural: `1.5 metres` or `0.75 metres`
-    /// If singular, `1 metre`
-    pub fn display(&self, decimals: usize) -> String {
-        let val = self.val.round_to(decimals);
-        if val == 1.0 {
-            self.display_singular(decimals)
-        } else {
-            self.display_plural(decimals)
-        }
-    }
-
-    /// Displays the value with the plural name after it (and a space between).
-    /// The value is rounded to the number of decimals (and will show trailing `0`)
-    /// The plurality of the value is not considered (see Dim.display(decimals)).
-    /// EX: `1.0 metres`, `1.5 metres`
-    pub fn display_plural(&self, decimals: usize) -> String {
-        let val = self.val.round_to(decimals);
-        format!("{:.*} {}", decimals, val, self.unit.plural)
-    }
-
-    /// Displays the value with the singular name after it (and a space between).
-    /// The value is rounded to the number of decimals (and will show trailing `0`)
-    /// The plurality of the value is not considered (see Dim.display(decimals)).
-    /// EX: `1.0 metre`, `1.5 metre`
-    pub fn display_singular(&self, decimals: usize) -> String {
-        let val = self.val.round_to(decimals);
-        format!("{:.*} {}", decimals, val, self.unit.singular)
-    }
-}
-// Setters here
-impl Dim {
-    /// Converts the value stored to the new unit and stores the unit
-    pub fn convert_to(&mut self, new_unit: &'static Unit) {
-        // Convert the value over
-        self.val = Dim::convert(self.val, self.unit, new_unit);
-        // Set the unit
-        self.unit = new_unit;
-    }
-}
-// Operators here
-impl Dim {
-    /// Adds the other value to this one
-    pub fn add_other(&mut self, other: &Dim) {
+    fn add_other(&mut self, other: &Length) {
         self.val += other.get_val_as(self.unit);
     }
-    /// Subtracts the other value from this one
-    pub fn subtract_other(&mut self, other: &Dim) {
+    fn subtract_other(&mut self, other: &Length) {
         self.val -= other.get_val_as(self.unit);
     }
 }
+// Implement the internal setters
+impl MeasureInternalSetters<Length> for Length {
+    fn set_unit_internal(&mut self, unit: &'static LengthUnit) {
+        self.unit = unit;
+    }
+    fn set_val_internal(&mut self, val: Float) {
+        self.val = val;
+    }
+}
+// Implement the conversion traits
+impl MeasureConvert<Length> for Length {}
 
-// Implement the various operators
-impl Add for Dim {
-    type Output = Dim;
-    fn add(self, other: Dim) -> Dim {
-        // Get these values as metre
-        let val = self.get_val_as(&METRE) + other.get_val_as(&METRE);
-        // Adjust them to the first value's unit
-        let val = Dim::convert(val, &METRE, self.unit);
-        Dim {
+// Add the operators
+impl Add<Length> for Length {
+    type Output = Length;
+    fn add(self, other: Length) -> Length {
+        let val = self.val + other.get_val_as(self.unit);
+        Length {
             val,
             unit: self.unit,
         }
     }
 }
-
-impl Sub for Dim {
-    type Output = Dim;
-    fn sub(self, other: Dim) -> Dim {
-        // Get these values as metre
-        let val = self.get_val_as(&METRE) - other.get_val_as(&METRE);
-        // Adjust them to the first value's unit
-        let val = Dim::convert(val, &METRE, self.unit);
-        Dim {
-            val,
-            unit: self.unit,
-        }
-    }
-}
-
-impl AddAssign for Dim {
-    fn add_assign(&mut self, other: Dim) {
-        // Simply use add_other
+impl AddAssign<Length> for Length {
+    fn add_assign(&mut self, other: Length) {
         self.add_other(&other);
     }
 }
-
-impl SubAssign for Dim {
-    fn sub_assign(&mut self, other: Dim) {
-        // Simply use add_other
+impl Sub<Length> for Length {
+    type Output = Length;
+    fn sub(self, other: Length) -> Length {
+        let val = self.val - other.get_val_as(self.unit);
+        Length {
+            val,
+            unit: self.unit,
+        }
+    }
+}
+impl SubAssign<Length> for Length {
+    fn sub_assign(&mut self, other: Length) {
         self.subtract_other(&other);
     }
 }
--- src/length/test.rs
+++ src/length/test.rs
@@ -1,15 +1,16 @@
 // Move on to the tests
-use super::Dim;
-use super::*;
+use super::units::si::*;
+use super::Length;
+use crate::base_types::*;
 
 #[test]
 fn test_add_sub() {
     // Start out with a basic number
-    let mut first = Dim::new(0.5, &METRE);
+    let mut first = Length::new(0.5, &METRE);
     // Ensure the most basic thing; that it actually is the same
     assert_eq!(first.get_val(), 0.5);
     // Create some kilometres
-    let second = Dim::new(0.75, &KILOMETRE);
+    let second = Length::new(0.75, &KILOMETRE);
     assert_eq!(second.get_val_as(&METRE), 750.0);
     assert_eq!(second.get_val_as(&CENTIMETRE), 75000.0);
     assert_eq!(second.get_val_as(&MILLIMETRE), 750000.0);
@@ -21,34 +22,34 @@ fn test_add_sub() {
     first -= second;
     assert_eq!(first.get_val(), 0.5);
     // Create one from millimetre
-    let third = Dim::new(17.3, &MILLIMETRE);
+    let third = Length::new(17.3, &MILLIMETRE);
     assert_eq!(third.get_val_as(&METRE), 0.0173);
     assert_eq!(third.get_val_as(&CENTIMETRE), 1.73);
     assert_eq!(third.get_val_as(&MILLIMETRE), 17.3);
 
     // Add to get a new value
-    let dim = second + third;
-    assert_eq!(dim.get_val(), 0.7500173);
-    let dim = second - third;
+    let length = second + third;
+    assert_eq!(length.get_val(), 0.7500173);
+    let length = second - third;
     // Subtract, as well
-    assert_eq!(dim.get_val(), 0.7499827);
+    assert_eq!(length.get_val(), 0.7499827);
 }
 #[test]
 fn test_str() {
     // Test the singular and plural
-    let val = Dim::new(1.0, &METRE);
+    let val = Length::new(1.0, &METRE);
     assert_eq!(val.display(0), "1 metre");
     assert_eq!(val.display(1), "1.0 metre");
     assert_eq!(val.display(2), "1.00 metre");
     assert_eq!(val.display_plural(2), "1.00 metres");
     assert_eq!(val.display_abbr(1), "1.0m");
 
-    let val = Dim::new(0.5, &METRE);
+    let val = Length::new(0.5, &METRE);
     assert_eq!(val.display(1), "0.5 metres");
     assert_eq!(val.display(2), "0.50 metres");
     assert_eq!(val.display_singular(1), "0.5 metre");
     // Test various rounding
-    let val = Dim::new(0.125, &METRE);
+    let val = Length::new(0.125, &METRE);
     assert_eq!(val.display(0), "0 metres");
     assert_eq!(val.display(1), "0.1 metres");
     assert_eq!(val.display(2), "0.13 metres");
@@ -56,7 +57,7 @@ fn test_str() {
     assert_eq!(val.display(4), "0.1250 metres");
     assert_eq!(val.display_abbr(3), "0.125m");
     // Check with a whole number
-    let val = Dim::new(24.0, &METRE);
+    let val = Length::new(24.0, &METRE);
     assert_eq!(val.display(0), "24 metres");
     assert_eq!(val.display(1), "24.0 metres");
     assert_eq!(val.display(2), "24.00 metres");
@@ -66,7 +67,7 @@ fn test_str() {
 }
 #[test]
 fn convert() {
-    let mut val = Dim::new(24.0, &KILOMETRE);
+    let mut val = Length::new(24.0, &KILOMETRE);
     // Best to ensure that we have a decent starting ground
     assert_eq!(val.get_val(), 24.0);
     // Check the number of millimetres
@@ -81,12 +82,12 @@ fn convert() {
 
 #[test]
 fn check_borrow() {
-    let mut val = Dim::new(125.0, &CENTIMETRE);
+    let mut val = Length::new(125.0, &CENTIMETRE);
     let val2 = val.clone();
     borrow(&mut val, &val2);
     assert_eq!(val.get_val(), 250.0);
 }
 // TODO: FIGURE OUT PROPER BORROW AND OPERATORS
-fn borrow(dim: &mut Dim, dim2: &Dim) {
-    dim.add_other(dim2);
+fn borrow(val: &mut Length, val2: &Length) {
+    val.add_other(val2);
 }
